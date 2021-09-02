237c2ca427893fef84a488f3629c1bdce77e1434
rohitpatil2137 <69861855+rohitpatil2137@users.noreply.github.com>
2021-09-02 17:22:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Best Coworking Space in Baner| Shared Office Space in Baner|Pune</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta name="keywords" content="Best coworking space in baner Pune, shared office space in Baner, Pune coworking for startups, 
+top coworking space in Pune, shared office space for rent, affordable coworker space in Baner Pune" />
+	<meta name="description" content="Venture Centre Network offers the best coworking spaces, virtual offices, shared office spaces, and conference rooms in Pune | Baner | Koregaon Park |Serviced Office Rent in Pune">
+	<link rel="canonical" href="https://venturecentre.in" />
+	<link href="img/favicon.png" rel="shortcut icon">
+	<link rel="apple-touch-icon" href="img/favicon.png">
+	<!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@200&display=swap" rel="stylesheet">
+	<!-- Bootstrap CSS File -->
+	
+	<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	
+	<!-- Main Stylesheet File -->
+	<link href="css/style.css" rel="stylesheet">
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="https://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.9/jquery-ui.js" type="text/javascript"></script>
+	<link href="https://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.9/themes/blitzer/jquery-ui.css" rel="stylesheet" type="text/css" />
+	<!--<meta name="description" content="Affordable coworker space"> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="stylesheet" href="assets1/css/bootstrap.min.css">
+	<!--        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">-->
+	<!--For Plugins external css-->
+	<link rel="stylesheet" href="assets1/css/plugins.css" />
+	<link rel="stylesheet" href="assets1/css/roboto-webfont.css" />
+	<!--Theme custom css -->
+	<link rel="stylesheet" href="assets1/css/style.css">
+	
+	<!--Theme Responsive css-->
+	<link rel="stylesheet" href="assets1/css/responsive.css" />
+	<script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119388570-1"></script>
+	<style>
+		*{
+		font-family: 'Roboto', sans-serif;
+		}
+	</style>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+		
+		  gtag('config', 'UA-119388570-1');
+	</script>
+	<!-- End of google analytics tracking code-->
+</head>
+
+<body>
+	<div id="preloader"></div>
+	<!--==========================
+
+  Hero Section
+
+============================-->
+	<section id="hero">
+		<div class="hero-container">
+			<div class="wow fadeIn">
+				<h1>Best Coworking Space in Baner | Shared Office in Baner | Pune 
+		<br> Venture Centre Network </h1>	
+				<h2>Affordable Coworking Space for <span class="rotating"> Entrepreneurs, Innovators, Designers, Writers, Freelancers </span> </h2>
+				<div class="actions"> <a href="tel:+91-7588991100" class="btn-get-started">Get Started <br> # 7588991100</br></a>	 <a href="#about" class="btn-pricing">Ready to Move, Fully Equipped <br>Premium co-working Spaces <br> at Prime locations with the best Prices </br></a>
+					<a href="https://vcncoworking.com/best-coworking-space-amenities-pune/" class="btn-services">Our Amenities <br> (Plug-n-Play)</br></a>	
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--==========================
+
+  Header Section
+
+============================-->
+	<header id="header">
+		<div class="container">
+			<div id="logo" class="pull-left">
+				<a href="#hero">
+					<img src="img/logo_vcn.png" alt="VCN Coworking Pune" title="" /></img>
+				</a>
+			</div>
+			<nav id="nav-menu-container">
+				<ul class="nav-menu">
+					<li class="menu-active"><a href="#hero">Home</a>
+					</li>
+					<li><a href="#about">About Us</a>
+					</li>
+					<li><a href="amenities.php">Amenities</a>
+					</li>
+					<li><a href="https://venturecentre.in/blog">Blog</a>
+					</li>
+					<!-- <li><a href="baner.php">Gallery</a></li>-->
+					<li><a href="locations.php">Pricing</a>
+						<li><a href="contact.php">Contact Us</a>
+						</li>
+				</ul>
+			</nav>
+			<!-- #nav-menu-container -->
+		</div>
+	</header>
+	<!-- #header -->
+	<!--==========================
+
+  About Section
+
+============================-->
+	<section id="about">
+		<div class="container wow fadeInUp">
+			<div class="row">
+				<div class="col-md-12">
+					<h3 class="section-title">VCN Shared Office Space in PUNE at Baner & Camp</h3>
+					<div class="section-title-divider"></div>
+					<p class="section-description">A Coworking place to Work, Connect, Collaborate and Rise.</p>
+					<br>
+					<br>
+				</div>
+			</div>
+		</div>
+		<div class="container about-container wow fadeInUp">
+			<div class="row">
+				<div class="col-md-6 col-md-push-6 about-content">
+					<p class="about-text">Our Co-working space is inspiring, productive and the best shared office space in Pune for Entrepreneurs, Start-ups, Freelancers and Individual Professionals. We offer the best pricing and the opportunity for coworker space that includes all the amenities with a dedicated desk with lock and key, high speed internet, meeting/conference rooms, open lounge, power backup and 24x7 security. No need to spend for a personal office because all this comes to you in a very reasonable, simple but with a collaborative corporate shared office space.</p>
+					<p class="about-text">VCN Coworking in Pune is a collaboration of people, ideas, skills, vision and aspirations that stimulates innovation for exchange and connectivity of entrepreneurial people. You will feel the energy and freshness for creativity to be at a place that is peaceful and quiet with openness on all the sides. A flexible and enriching environment far beyond just coworking with the engaging and inspiring atmosphere at VCN co-working space helps you to be productive and elevate. Our premium coworking places at the prime locations in Pune are at Baner and Camp.</p>
+					<p class="about-text">VCN Coworking space is the most preferred coworkers space with plug and play for growing your business and best alternative to working remotely for the students, corporates, IT professional, entrepreneurs, designers, startups and freelancers...</p>
+				</div>
+			</div>
+		</div>
+		<br>
+		<br>
+		<section id="about1">
+			<div class="container about1-container wow fadeInUp">
+				<div class="row">
+					<div class="col-md-6 about1-content col-md-6">
+						<h3 class="about1-title">Benefits of Our Coworking Spaces</h3>
+						<p class="about1-text">At Venture Centre Network Coworking Spaces, we provide a persistent & energy-filled environment that would maximise your productivity. We have shared offices at premium locations in Pune with the top class amenities. We value collaboration, more than competition. At VCN Coworking we will help you to bring creativity to your endeavours.</p>
+						<p class="about1-text">Our coworking spaces have stylish and relaxed environments giving you enriching and lively experience that produces the best quality of work life. We have flexible, affordable and community centric work places ideal for entrepreneurial minds to focus on their work and iterate their ideas to get the best output reaching perfection. Our shared office paces have plenty of natural light and it can accommodate small and large teams alike. .</p>
+						<p class="about1-text">Joyful Coworking !!</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- // END : column //  -->
+		</div>
+		<!-- // END : row //  -->
+		<!-- end new section-->
+		<script type="application/ld+json">
+			{
+			    "@context": "http://schema.org",
+			    "@type": "Organization",
+			    "name": "VCN Coworking Space",
+			    "url": "https://venturecentre.in",
+			    "address": "301/302/303, Avenue Imperia, Above FabIndia, Baner, Pune 411045",
+			    "sameAs": [
+			      "https://www.facebook.com/vcncoworking/",
+			      "https://twitter.com/VCNcoworking",
+			      "https://www.instagram.com/vcn.coworkingspace/",
+			      "https://www.linkedin.com/company/vcncoworking/?viewAsMember=true"
+			    ]
+			  }
+		</script>
+		<!--==========================
+
+  Footer
+
+============================-->
+		<section id="subscribe">
+			<div class="container wow fadeInUp">
+				<div class="row">
+					<div class="col-md-4">
+						<h3 class="subscribe-title">VCN Co-working Space</h3>
+						<h4 class="subscribe-text"> Best coworking space in Pune
+		  <br> Mobile: +91 - 8983 112200/ 758899 1100 </br>  Mail: contact@venturecentre.in</h4>	
+						<div>
+							<br>
+							<a href="https://www.facebook.com/vcncoworking/" target="_blank">
+								<img src="img/fb.png" alt="coworking.fb">
+								<a href="https://www.linkedin.com/company/vcncoworking" target="_blank">
+									<img src="img/linkedin.png" alt="coworking.LIn">
+									<a href="https://twitter.com/VCNcoworking" target="_blank">
+										<img src="img/twitter.png" alt="coworking.Tr">
+										<a href="https://www.instagram.com/vcn.coworking.space/" target="_blank">
+											<img src="img/instagram.png" alt="coworking.Ig">
+											</br>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="copyright">&copy; 2019 Copyright VCN. All Rights Reserved</div>
+					</div>
+				</div>
+			</div>
+		</footer> <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+		<!-- Required JavaScript Libraries -->
+		<script src="lib/morphext/morphext.min.js"></script>
+  <script src="lib/stickyjs/sticky.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+		<!-- Template Specisifc Custom Javascript File -->
+		<script src="js/custom.js"></script>
+  <script src="contactform/contactform.js"></script>
+	<script src="assets1/js/plugins.js"></script>
+</body>
+
+</html>
